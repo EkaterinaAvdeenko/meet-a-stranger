a3750a5482cc6f0a19f1e0a633f7659924f94a65
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 18:03:25 +0100

    meet-a-stranger_AvdeenkoEkaterina

--- src/test/java/com/epam/rd/autotasks/meetastranger/MeetAStrangerTest.java
+++ src/test/java/com/epam/rd/autotasks/meetastranger/MeetAStrangerTest.java
@@ -1,5 +1,7 @@
 package com.epam.rd.autotasks.meetastranger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,36 +14,35 @@ import java.util.stream.Stream;
 
 public class MeetAStrangerTest {
 
-    @ParameterizedTest
-    @MethodSource("dataProvider")
-    public void correctOutput(String expected, String userInput) {
+  private static Stream<Arguments> dataProvider() {
+    return Stream.of(
+        Arguments.of("Hello, Stranger", "Stranger"),
+        Arguments.of("Hello, Mister Stranger", "Mister Stranger"),
+        Arguments.of("Hello, Robert Downey Jr", "Robert Downey Jr")
+    );
+  }
 
-        ByteArrayInputStream in = new ByteArrayInputStream(userInput.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(out);
-        System.setOut(printStream);
-        PrintStream defaultOut = System.out;
-        InputStream defaultIn = System.in;
+  @ParameterizedTest
+  @MethodSource("dataProvider")
+  public void correctOutput(String expected, String userInput) {
 
-        try {
-            MeetAStranger.main(new String[]{});
-            printStream.flush();
-            String actual = out.toString().trim();
-            assertEquals(expected, actual);
-        }
-        finally {
-            System.setIn(defaultIn);
-            System.setOut(defaultOut);
-        }
+    ByteArrayInputStream in = new ByteArrayInputStream(userInput.getBytes());
+    System.setIn(in);
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(out);
+    System.setOut(printStream);
+    PrintStream defaultOut = System.out;
+    InputStream defaultIn = System.in;
+
+    try {
+      MeetAStranger.main(new String[]{});
+      printStream.flush();
+      String actual = out.toString().trim();
+      assertEquals(expected, actual);
+    } finally {
+      System.setIn(defaultIn);
+      System.setOut(defaultOut);
     }
-
-    private static Stream<Arguments> dataProvider() {
-        return Stream.of(
-                Arguments.of("Hello, Stranger", "Stranger"),
-                Arguments.of("Hello, Mister Stranger", "Mister Stranger"),
-                Arguments.of("Hello, Robert Downey Jr", "Robert Downey Jr")
-        );
-    }
+  }
 }
 
